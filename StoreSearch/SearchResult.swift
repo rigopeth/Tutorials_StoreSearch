1c8a72099cf2f274d927b81a1398f1c49d63f784
rigopeth <rigo.dominguezg@gmail.com>
2023-03-08 22:36:56 -0600

    Add a search bar and table view. The search puts fake results in the table for now

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,13 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Rigoberto Dominguez on 08/03/23.
+//
+
+import Foundation
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
